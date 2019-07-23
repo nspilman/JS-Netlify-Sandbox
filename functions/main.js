ec229e1c43c3da48b71574545da6a1478d1584ad
Nate Spilman <natespilman@Nates-MacBook-Pro.local>
2019-07-22 21:01:31 -0700

    learnin'

--- functions/main.js
+++ functions/main.js
@@ -1,6 +1,8 @@
+
 exports.handler = function (event, context, callback) {
+
     callback(null, {
-        statusCode: 404,
-        body: "Hello, world!"
+        statusCode: 200,
+        body: context
     })
 }
